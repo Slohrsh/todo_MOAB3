eee0a0db8d8f45160a0a799b967302d5329c5340
Slohrsh <sebastian.lohr@hs-weingarten.de>
2017-01-19 17:54:56 +0100

    Change view after click on login

--- public/javascripts/logInController.js
+++ public/javascripts/logInController.js
@@ -7,11 +7,11 @@
 'use strict';
 var todoApp = angular.module("todo", ['ngMaterial']);
 
-todoApp.controller("logInController", ['$scope', '$http', '$location', function ($scope, $http, $location) {
+todoApp.controller("logInController", ['$scope', '$http', '$window', function ($scope, $http, $window) {
     //login Abfragen
     $scope.verifyCredentials = function(){
         /*$scope.user.passwd;
         $scope.user.name;*/
-        $location.path("todoUI"); // path not hash
+        $window.location = "todoUI";
     }
 }]);
