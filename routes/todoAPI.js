c81849bf9ac93611d6b19ed5b27dfda51ea1e5ee
Slohrsh <sebastian.lohr@hs-weingarten.de>
2017-01-21 19:55:19 +0100

    provide userid

--- routes/todoAPI.js
+++ routes/todoAPI.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
-var exchangeUserID = require('exchangeUserID');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'user_db',
